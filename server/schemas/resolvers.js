82d729eacb2ab939a23fe2427d89277e586dcb67
Thuy Nguyen <ttruong06@gmail.com>
2022-07-11 16:56:44 -0400

    modified typeDefs, resolvers, and index.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,17 @@
-// set up the resolver that will serve the response for the helloWorld query
+// set up the resolver that will serve the response for the query defined in 'typeDefs.js' file
+
+//import Thought
+const { User, Thought } = require('../models');
 
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
+        //we pass in the parent as more of a placeholder parameter. It won't be used, but we need something in that first parameter's spot so we can access the username argument from the second parameter
+      thoughts: async (parent, { username }) => {
+
+        //use a ternary operator to check if username exists. If it does, we set params to an object with a username key set to that value. If it doesn't, we simply return an empty object.
+        const params = username ? { username } : {};
+        //.find is to query thought;  .sort is desc. order
+        return Thought.find(params).sort({ createdAt: -1 })
       }
     }
   };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,26 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs; type Query{} defines a Query; ex: create Query: helloWorld as a string
+// create our typeDefs; type Query{} defines a Query; ex: create Query: helloWorld as a string; Query with [] will return an array
 const typeDefs = gql`
+type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
 type Query {              
-    helloWorld: String
+    thoughts(username: String): [Thought]
 }`;
 
 // export the typeDefs
