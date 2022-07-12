6eff201e7d1054a84f4ecacad6eed25086d0b9d7
Thuy Nguyen <ttruong06@gmail.com>
2022-07-12 13:38:19 -0400

    set up user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,13 +6,33 @@ const { User, Thought } = require('../models');
 const resolvers = {
     Query: {
         //we pass in the parent as more of a placeholder parameter. It won't be used, but we need something in that first parameter's spot so we can access the username argument from the second parameter
-      thoughts: async (parent, { username }) => {
+        thoughts: async (parent, { username }) => {
+            
 
         //use a ternary operator to check if username exists. If it does, we set params to an object with a username key set to that value. If it doesn't, we simply return an empty object.
         const params = username ? { username } : {};
         //.find is to query thought;  .sort is desc. order
         return Thought.find(params).sort({ createdAt: -1 })
-      }
+      },
+      thought: async (parent, { _id }) => {
+        return Thought.findOne({ _id });
+      },
+
+      // get all users
+    users: async () => {
+        return User.find()
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+        return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+      
     }
   };
   
