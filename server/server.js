138075600ca10a26aef83fd3db4b4d0a1df17633
Thuy Nguyen <ttruong06@gmail.com>
2022-07-13 22:47:52 -0400

    modified package.json and server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require('path');
 
-
-
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
 //import middleware function
