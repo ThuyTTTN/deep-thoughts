dc223dc7475a572e4ea14c068f58c291a2316cc9
Thuy Nguyen <ttruong06@gmail.com>
2022-07-14 12:09:39 -0400

    forms :

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 //import ThoughtList
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
+import ThoughtForm from '../components/ThoughtForm';
 
 import Auth from '../utils/auth';
 //allows us to make requests to the GraphQL server
@@ -23,8 +24,13 @@ const { data: userData } = useQuery(QUERY_ME_BASIC);
 
   return (
     <main>
-      <div className="flex-row justify-space-between">
-        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+  <div className="flex-row justify-space-between">
+    {loggedIn && (
+      <div className="col-12 mb-3">
+        <ThoughtForm />
+      </div>
+    )}
+    <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
